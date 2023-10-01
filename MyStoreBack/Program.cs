7e7d3e1a567515547d087f6771f0630d53fd0e70
Narkobaron15 <oleksii120@gmail.com>
2023-10-01 18:07:18 +0300

    Fetched pic on a mobile
    Minor changes on backend

---
+++ MyStoreBack/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+app.UseCors(policyBuilder => policyBuilder
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+);
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(path),
+    RequestPath = "/api/uploads",
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- MyStoreClient/app/src/main/java/com/example/mystore/MainActivity.kt
+++ MyStoreClient/app/src/main/java/com/example/mystore/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mystore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val imageUrl = "https://vpd121.itstep.click/images/1.jpg"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView);
+
+        Picasso.get().load(imageUrl).into(imageView);
     }
 }
